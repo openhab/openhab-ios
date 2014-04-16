6df61377e25dc8a369a019ce2af195861365a88a
belovictor <Rfhfns11>
2014-04-16 15:08:30 +0400

    Implemented selection widget

---
+++ openHAB/OpenHABSelectionTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  OpenHABSelectionTableViewController.h
+//  openHAB
+//
+//  Created by Victor Belov on 16/04/14.
+//  Copyright (c) 2014 Victor Belov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OpenHABSelectionTableViewController : UITableViewController
+
+@end
