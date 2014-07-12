0c6dcc315e64381791cdafcec360883e486a0e2e
belovictor <Rfhfns11>
2014-07-12 11:31:30 +0400

    Fixed image refresh

--- openHAB/ImageUITableViewCell.m
+++ openHAB/ImageUITableViewCell.m
@@ -13,6 +13,8 @@
 @implementation ImageUITableViewCell
 @synthesize widgetImage, delegate;
 
+NSTimer *refreshTimer;
+
 - (void)loadWidget:(OpenHABWidget *)widgetToLoad
 {
     self.widget = widgetToLoad;
@@ -23,18 +25,48 @@
 {
     widgetImage = (UIImageView*)[self viewWithTag:901];
     if (widget.image == nil) {
-        int random = arc4random() % 1000;
-        [widgetImage setImageWithURL:[NSURL URLWithString:[NSString stringWithFormat:@"%@&random=%d", self.widget.url, random]] placeholderImage:nil options:SDWebImageCacheMemoryOnly completed:^(UIImage *image, NSError *error, SDImageCacheType cacheType) {
+        [self loadImage];
+    } else {
+        [widgetImage setImage:widget.image];
+    }
+    // If widget have a refresh rate configured, schedule an image update timer
+    if (self.widget.refresh != nil && refreshTimer == nil) {
+        NSTimeInterval refreshInterval = [self.widget.refresh floatValue] / 1000;
+        refreshTimer = [NSTimer scheduledTimerWithTimeInterval:refreshInterval
+                                                        target:self
+                                                      selector:@selector(refreshImage:)
+                                                      userInfo:nil
+                                                       repeats:YES];
+    }
+}
+
+- (void)loadImage
+{
+    int random = arc4random() % 1000;
+    [widgetImage setImageWithURL:[NSURL URLWithString:[NSString stringWithFormat:@"%@&random=%d", self.widget.url, random]] placeholderImage:nil options:SDWebImageCacheMemoryOnly completed:^(UIImage *image, NSError *error, SDImageCacheType cacheType) {
         widget.image = self.widgetImage.image;
         [widgetImage setFrame:self.contentView.frame];
         if (self.delegate != nil) {
             [self.delegate didLoadImage];
         }
-        }];
-    } else {
-        [widgetImage setImage:widget.image];
-    }
+    }];
 }
 
+- (void)refreshImage:(NSTimer *)timer
+{
+    int random = arc4random() % 1000;
+    [widgetImage setImageWithURL:[NSURL URLWithString:[NSString stringWithFormat:@"%@&random=%d", self.widget.url, random]] placeholderImage:self.widgetImage.image options:SDWebImageCacheMemoryOnly completed:^(UIImage *image, NSError *error, SDImageCacheType cacheType) {
+        widget.image = self.widgetImage.image;
+    }];
+}
+
+- (void)willMoveToWindow:(UIWindow *)newWindow
+{
+    [super willMoveToWindow:newWindow];
+    if (newWindow == nil && refreshTimer != nil) {
+        [refreshTimer invalidate];
+        refreshTimer = nil;
+    }
+}
 
 @end
