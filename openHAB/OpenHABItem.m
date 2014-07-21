0bd6dd1653fa447873490c2dca5ad9f13a08007c
belovictor <Rfhfns11>
2014-07-21 23:14:15 +0400

    Fixed a bug which led to a crash when ColorItem had an invalid value

--- openHAB/OpenHABItem.m
+++ openHAB/OpenHABItem.m
@@ -38,11 +38,15 @@
         return [UIColor colorWithHue:0 saturation:0 brightness:0 alpha:1.0];
     } else {
         NSArray *values = [state componentsSeparatedByString:@","];
-        CGFloat hue = [(NSString*)[values objectAtIndex:0] floatValue]/360;
-        CGFloat saturation = [(NSString*)[values objectAtIndex:1] floatValue]/100;
-        CGFloat brightness = [(NSString*)[values objectAtIndex:2] floatValue]/100;
-        NSLog(@"%f %f %f", hue, saturation, brightness);
-        return [UIColor colorWithHue:hue saturation:saturation brightness:brightness alpha:1.0];
+        if ([values count] == 3) {
+            CGFloat hue = [(NSString*)[values objectAtIndex:0] floatValue]/360;
+            CGFloat saturation = [(NSString*)[values objectAtIndex:1] floatValue]/100;
+            CGFloat brightness = [(NSString*)[values objectAtIndex:2] floatValue]/100;
+            NSLog(@"%f %f %f", hue, saturation, brightness);
+            return [UIColor colorWithHue:hue saturation:saturation brightness:brightness alpha:1.0];
+        } else {
+            return [UIColor colorWithHue:0 saturation:0 brightness:0 alpha:1.0];
+        }
     }
 }
 
