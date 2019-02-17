54da899b336c5da4566639e9ff8a020150ad881b
Tim Müller-Seydlitz <timbms@gmail.com>
2019-02-17 17:27:15 +0100

    Committing back Podfile.lock

--- ImageUINewTableViewCell.swift
+++
@@ -1,9 +0,0 @@
-//
-//  ImageUINewTableViewCell.swift
-//  openHAB
-//
-//  Created by Tim Müller-Seydlitz on 16.02.19.
-//  Copyright © 2019 openHAB e.V. All rights reserved.
-//
-
-import Foundation
