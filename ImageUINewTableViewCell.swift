5beb4347ee0518f54e44d119ffe031e8baf55287
Tim Müller-Seydlitz <timbms@gmail.com>
2019-02-17 17:15:03 +0100

    Added ImageUINewTableViewCell
    Signed-off-by: Tim Müller-Seydlitz <timbms@gmail.com>

---
+++ ImageUINewTableViewCell.swift
@@ -0,0 +1,9 @@
+//
+//  ImageUINewTableViewCell.swift
+//  openHAB
+//
+//  Created by Tim Müller-Seydlitz on 16.02.19.
+//  Copyright © 2019 openHAB e.V. All rights reserved.
+//
+
+import Foundation
