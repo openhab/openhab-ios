ae24802e056fa2e44cb425b8c923f74ce2912cc6
Tim Müller-Seydlitz <timbms@gmail.com>
2019-03-04 08:22:09 +0100

    Getting rid of hexString
    Signed-off-by: Tim Müller-Seydlitz <timbms@gmail.com>

--- openHABTestsSwift/OpenHABTestsSwift.swift
+++ openHABTestsSwift/OpenHABTestsSwift.swift
@@ -59,9 +59,11 @@ class OpenHABTestsSwift: XCTestCase {
         }
     }
 
-//    func testHexString() {
-//        let str = Data("Tim iPhone"
-//        var hex = str.hexString()
-//        XCTAssert (hex == "00020", "hex properly calculated")
-//    }
+    func testHexString() {
+        let iPhoneData: Data = "Tim iPhone".data(using: .utf8)!
+        let hex = iPhoneData.hexString().uppercased()
+        let hexWithReduce = iPhoneData.reduce("", {$0 + String(format: "%02X", $1)})
+        XCTAssert (hex == "54696D206950686F6E65", "hex properly calculated")
+        XCTAssert (hexWithReduce == "54696D206950686F6E65", "hex properly calculated with reduce")
+    }
 }
