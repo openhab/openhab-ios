3e38c9ca4ffc1096fcfeb997cde37f11a3b420ad
digitaldan <dan@digitaldan.com>
2019-04-12 17:02:32 -0700

    Fixes an issue were we would never use the local URL due to the fact that URLRquests are async and the call to "resume" completes instantly, so we were always returning false when calling isURLReachable
    Signed-off-by: digitaldan <dan@digitaldan.com>

--- openHAB/OpenHABTracker.swift
+++ openHAB/OpenHABTracker.swift
@@ -54,15 +54,16 @@ class OpenHABTracker: NSObject, NetServiceDelegate, NetServiceBrowserDelegate {
                     // If it is WiFi
                 } else {
                     os_log("OpenHABTracker network is Wifi", log: .default, type: .info)
-                    // Check if local URL is configured, if yes
+                    // Check if local URL is configured
                     if openHABLocalUrl.count > 0 {
-                        //if Reachability(hostname: openHABLocalUrl) {
-                        if isURLReachable(URL(string: openHABLocalUrl)) {
-                            trackedLocalUrl()
-                        } else {
-                            trackedRemoteUrl()
-                        }
-                        // If not, go for Bonjour discovery
+                        let request = URLRequest(url: URL(string: openHABLocalUrl)!, cachePolicy: .reloadIgnoringCacheData, timeoutInterval: 2.0)
+                        URLSession.shared.dataTask(with: request, completionHandler: { data, response, error -> Void in
+                            if error == nil {
+                                self.trackedLocalUrl()
+                            } else {
+                                self.trackedRemoteUrl()
+                            }
+                        }).resume()
                     } else {
                         startDiscovery()
                     }
@@ -190,24 +191,6 @@ class OpenHABTracker: NSObject, NetServiceDelegate, NetServiceBrowserDelegate {
         let socketAddress: sockaddr_in = data.castToCPointer()
         ipString = String(cString: inet_ntoa(socketAddress.sin_addr), encoding: .ascii)  ///problem here
         return ipString
-    }
-
-    func isURLReachable(_ url: URL?) -> Bool {
-        var result: Bool = false
-
-        if let url = url {
-            let request = URLRequest(url: url, cachePolicy: .reloadIgnoringCacheData, timeoutInterval: 2.0)
-            let session = URLSession.shared
-            let task = session.dataTask(with: request,
-                                        completionHandler: { data, response, error -> Void in
-                                            if error == nil {
-                                                result = true
-                                            } else {
-                                                result = false
-                                            }})
-            task.resume()
-        }
-        return result
     }
 
     func string(from status: Reachability.Connection) -> String? {
