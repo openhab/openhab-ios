1d06872d4e72a2e9d902ce38ebb3f51a95cc0957
Tim Müller-Seydlitz <timbms@gmail.com>
2019-08-15 16:26:36 +0200

    XML parsing reactivated  - starting with OpenHABSitemap ( to be continued for OpenHABSitemapPage)  and testing
    Signed-off-by: Tim Müller-Seydlitz <timbms@gmail.com>

---
+++ openHAB/NetworkConnection.swift
@@ -0,0 +1,66 @@
+//
+//  BetworkConnection.swift
+//  openHAB
+//
+//  Created by Tim Müller-Seydlitz on 10/08/19.
+//  Copyright (c) 2019 David O'Neill. All rights reserved.
+
+import Alamofire
+import os.log
+
+// https://medium.com/@AladinWay/write-a-networking-layer-in-swift-4-using-alamofire-5-and-codable-part-2-perform-request-and-b5c7ee2e012d
+// Transition from AFNetworking to Alamofire 5.0
+// SessionManager --> Session
+// serverTrustPolicyManager --> serverTrustManager
+// ServerTrustPolicyManager --> ServerTrustManager
+
+class NetworkConnection {
+
+    static var clientCertificateManager: ClientCertificateManager = ClientCertificateManager()
+
+    var manager: Alamofire.SessionManager!
+    static let shared = NetworkConnection()
+
+    //let UIImageView.af_sharedImageDownloader = ImageDownloader(sessionManager: manager)
+
+    init() {
+
+        manager = Alamofire.SessionManager(configuration: URLSessionConfiguration.default, delegate: SessionDelegate(), serverTrustPolicyManager: AlamofireRememberingSecurityPolicy(ignoreCertificates: true) )
+
+        manager.delegate.sessionDidReceiveChallenge = { session, challenge in
+            var disposition: URLSession.AuthChallengeDisposition = .performDefaultHandling
+            var credential: URLCredential?
+
+            if challenge.protectionSpace.authenticationMethod == NSURLAuthenticationMethodServerTrust {
+                disposition = URLSession.AuthChallengeDisposition.useCredential
+                credential = URLCredential(trust: challenge.protectionSpace.serverTrust!)
+            } else if challenge.protectionSpace.authenticationMethod == NSURLAuthenticationMethodClientCertificate {
+                (disposition, credential) = self.evaluateClientTrust(challenge: challenge)
+            } else {
+                if challenge.previousFailureCount > 0 {
+                    disposition = .cancelAuthenticationChallenge
+                } else {
+                    credential = self.manager.session.configuration.urlCredentialStorage?.defaultCredential(for: challenge.protectionSpace)
+
+                    if credential != nil {
+                        disposition = .useCredential
+                    }
+                }
+            }
+
+            return (disposition, credential)
+        }
+    }
+
+    func evaluateClientTrust(challenge: URLAuthenticationChallenge) -> (URLSession.AuthChallengeDisposition, URLCredential?) {
+        let dns = challenge.protectionSpace.distinguishedNames
+        if let dns = dns {
+            let identity = NetworkConnection.clientCertificateManager.evaluateTrust(distinguishedNames: dns)
+            if let identity = identity {
+                let credential = URLCredential.init(identity: identity, certificates: nil, persistence: URLCredential.Persistence.forSession)
+                return (.useCredential, credential)
+            }
+        }
+        return (.cancelAuthenticationChallenge, nil)
+    }
+}
---
+++ openHABTestsSwift/OpenHABFormatTests.swift
@@ -0,0 +1,84 @@
+//
+//  OpenHABFormatTests.swift
+//  openHABTestsSwift
+//
+//  Created by Tim Müller-Seydlitz on 09.08.19.
+//  Copyright © 2019 openHAB e.V. All rights reserved.
+//
+
+import XCTest
+
+class OpenHABFormatTests: XCTestCase {
+
+    override func setUp() {
+        super.setUp()
+    }
+
+    func testValueToText() {
+        func valueText(_ widgetValue: Double, step: Double) -> String {
+            let digits = max (-Decimal(step).exponent, 0)
+            let numberFormatter = NumberFormatter()
+            numberFormatter.minimumFractionDigits = digits
+            numberFormatter.decimalSeparator  = "."
+            return numberFormatter.string(from: NSNumber(value: widgetValue)) ?? ""
+        }
+
+        func valueTextWithoutFormatter(_ widgetValue: Double, step: Double) -> String {
+            let digits = max (-Decimal(step).exponent, 0)
+            return String(format: "%.\(digits)f", widgetValue)
+        }
+
+        XCTAssertEqual(valueText(1000.0, step: 5.23), "1000.00")
+        XCTAssertEqual(valueText(1000.0, step: 1), "1000")
+        XCTAssertEqual(valueTextWithoutFormatter(1000.0, step: 5.23), "1000.00")
+
+    }
+
+    func testXMLSitemapDecoder() {
+        let json = """
+<sitemaps><sitemap>
+    <name>default</name>
+    <label>Welcome Home</label>
+    <link>http://192.168.170.5:8080/rest/sitemaps/default</link>
+    <homepage><link>http://192.168.170.5:8080/rest/sitemaps/default/default</link>
+    <leaf>false</leaf></homepage>
+    </sitemap></sitemaps>
+""".data(using: .utf8)!
+
+        var sitemaps = [OpenHABSitemap]()
+
+        if let doc: GDataXMLDocument? = try? GDataXMLDocument(data: json) {
+            if doc?.rootElement().name() == "sitemaps" {
+                for element in doc?.rootElement().elements(forName: "sitemap") ?? [] {
+                    if let element = element as? GDataXMLElement {
+                        let sitemap = OpenHABSitemap(xml: element)
+                        sitemaps.append(sitemap)
+                    }
+                }
+            }
+        }
+        XCTAssert(sitemaps[0].homepageLink == "http://192.168.170.5:8080/rest/sitemaps/default/default", "JSON Sitemap properly parsed")
+
+    }
+
+    func testXMLSitemapPageDecoder() {
+        let json = """
+<sitemap><name>default</name><label>Križ 62a</label><link>http://192.168.0.249:8080/rest/sitemaps/default</link><homepage><id>default</id><title>Križ 62a</title><link>http://192.168.0.249:8080/rest/sitemaps/default/default</link><leaf>false</leaf><widget><widgetId>default_0</widgetId><type>Frame</type><label/><icon>frame</icon><widget><widgetId>default_0_0</widgetId><type>Text</type><label>Nadstropje [21.2 °C]</label><icon>attic</icon><valuecolor>#008000</valuecolor><item><type>NumberItem</type><name>Office_Temperature</name><state>21.20</state><link>http://192.168.0.249:8080/rest/items/Office_Temperature</link></item>
+""".data(using: .utf8)!
+
+        var sitemaps = [OpenHABSitemap]()
+
+        if let doc: GDataXMLDocument? = try? GDataXMLDocument(data: json) {
+            if doc?.rootElement().name() == "sitemaps" {
+                for element in doc?.rootElement().elements(forName: "sitemap") ?? [] {
+                    if let element = element as? GDataXMLElement {
+                        let sitemap = OpenHABSitemap(xml: element)
+                        sitemaps.append(sitemap)
+                    }
+                }
+            }
+        }
+        XCTAssert(sitemaps[0].homepageLink == "http://192.168.170.5:8080/rest/sitemaps/default/default", "JSON Sitemap properly parsed")
+
+
+}
