133c22b89dbe16ffe7a639c6a9b4f9e896108fd4
David O'Neill <ufodone@gmail.com>
2019-08-17 20:39:48 +0200

    Icons not displayed on openHAB iOS app 2.1.x when connected via HTTPS (openhab#353) * Consolidated authentication challenge code between AFNetworking and SDWebImage to ensure server and client cert handling is done consistently between both networking packages.
    Signed-off-by: David O'Neill <ufodone@gmail.com>

--- openHAB/OpenHABHTTPRequestOperation.swift
+++ openHAB/OpenHABHTTPRequestOperation.swift
@@ -14,40 +14,30 @@ class OpenHABHTTPRequestOperation: AFHTTPRequestOperation {
     init(request: URLRequest, delegate: AFRememberingSecurityPolicyDelegate?) {
         super.init(request: request)
         super.setWillSendRequestForAuthenticationChallenge { (connection: NSURLConnection, challenge: URLAuthenticationChallenge) in
-
-            if challenge.protectionSpace.authenticationMethod == NSURLAuthenticationMethodServerTrust {
-                if self.securityPolicy.evaluateServerTrust(challenge.protectionSpace.serverTrust!, forDomain: challenge.protectionSpace.host) {
-                    let credential = URLCredential.init(trust: challenge.protectionSpace.serverTrust!)
-                    challenge.sender!.use(credential, for: challenge)
-                } else {
-                    challenge.sender!.cancel(challenge)
-                }
-            } else if challenge.protectionSpace.authenticationMethod == NSURLAuthenticationMethodClientCertificate {
-                self.evaluateClientTrust(challenge: challenge)
-            } else {
-                if challenge.previousFailureCount == 0 {
-                    if self.credential != nil {
-                        challenge.sender!.use(self.credential!, for: challenge)
-                    } else {
-                        challenge.sender!.continueWithoutCredential(for: challenge)
-                    }
-                } else {
-                    challenge.sender!.continueWithoutCredential(for: challenge)
-                }
+			let policy = self.securityPolicy as! AFRememberingSecurityPolicy
+			let result = policy.handleAuthenticationChallenge(challenge: challenge)
+            switch result.0 {
+            case .useCredential:
+                challenge.sender!.use(result.1!, for: challenge)
+            case .cancelAuthenticationChallenge:
+                challenge.sender!.cancel(challenge)
+            case .rejectProtectionSpace:
+                break
+            default:
+				if challenge.previousFailureCount == 0 {
+					if self.credential != nil {
+						challenge.sender!.use(self.credential!, for: challenge)
+					} else {
+						challenge.sender!.continueWithoutCredential(for: challenge)
+					}
+				} else {
+					challenge.sender!.continueWithoutCredential(for: challenge)
+				}
             }
         }
 
-        let policy = AFRememberingSecurityPolicy(pinningMode: AFSSLPinningMode.none)
+        let policy = AFRememberingSecurityPolicy()
         policy.delegate = delegate
-
-        let prefs = UserDefaults.standard
-        let ignoreSSLCertificate = prefs.bool(forKey: "ignoreSSL")
-
-        if ignoreSSLCertificate {
-            os_log("Warning - ignoring invalid certificates", log: OSLog.remoteAccess, type: .info)
-            policy.allowInvalidCertificates = true
-            policy.validatesDomainName = false
-        }
         self.securityPolicy = policy
     }
 
@@ -55,17 +45,4 @@ class OpenHABHTTPRequestOperation: AFHTTPRequestOperation {
         fatalError("init(coder:) has not been implemented")
     }
 
-    func evaluateClientTrust(challenge: URLAuthenticationChallenge) {
-        let dns = challenge.protectionSpace.distinguishedNames
-        if let dns = dns {
-            let identity = OpenHABHTTPRequestOperation.clientCertificateManager.evaluateTrust(distinguishedNames: dns)
-            if let identity = identity {
-                let credential = URLCredential.init(identity: identity, certificates: nil, persistence: URLCredential.Persistence.forSession)
-                challenge.sender!.use(credential, for: challenge)
-                return
-            }
-        }
-        // No client certificate available
-        challenge.sender!.cancel(challenge)
-    }
 }
