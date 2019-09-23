60c49b5047cc2eb19a1ab5b4aacb475ad76d2a4d
Tim Müller-Seydlitz <timbms@gmail.com>
2019-09-23 23:03:55 +0200

    Throttling the flow of events from colorpicker to max once every 0.3 s
    Signed-off-by: Tim Müller-Seydlitz <timbms@gmail.com>

---
+++ openHAB/Throttler.swift
@@ -0,0 +1,33 @@
+//
+//  Throttler.swift
+//  openHAB
+//
+//  Created by Tim Müller-Seydlitz on 23.09.19.
+//  Copyright © 2019 openHAB e.V. All rights reserved.
+//
+
+import Foundation
+
+// Inspired by http://danielemargutti.com/2017/10/19/throttle-in-swift/
+
+public class Throttler {
+    private let queue: DispatchQueue = .global(qos: .background)
+
+    private var job = DispatchWorkItem {}
+    private var previousRun: Date = .distantPast
+    private var maxInterval: TimeInterval
+
+    init(seconds: Double) {
+        maxInterval = seconds
+    }
+
+    func throttle(block: @escaping () -> Void) {
+        job.cancel()
+        job = DispatchWorkItem { [weak self] in
+            self?.previousRun = Date()
+            block()
+        }
+        let delay = Date().timeIntervalSince(previousRun) > maxInterval ? 0 : maxInterval
+        queue.asyncAfter(deadline: .now() + Double(delay), execute: job)
+    }
+}
