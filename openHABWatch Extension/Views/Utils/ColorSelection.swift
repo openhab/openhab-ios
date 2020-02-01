24c10579fd47b0f9f13fa913a5cd24529a3526b9
Tim Müller-Seydlitz <timbms@gmail.com>
2020-02-01 22:59:59 +0100

    ColorPicker for SwiftUI: thumb now properly inside in circle (though not yet overlapping with underlying circle
    Signed-off-by: Tim Müller-Seydlitz <timbms@gmail.com>

--- openHABWatch Extension/Views/Utils/ColorSelection.swift
+++ openHABWatch Extension/Views/Utils/ColorSelection.swift
@@ -50,29 +50,23 @@ enum DragState {
 
 // swiftlint:disable file_types_order
 struct ColorSelection: View {
-    @GestureState var satBrightState: DragState = .inactive
+    @GestureState var thumb: DragState = .inactive
 
     @State var hue: Double = 0.5
-    @State var saturation: Double = 0.5
-    @State var brightness: Double = 0.5
+    @State var xpos: Double = 100
+    @State var ypos: Double = 100
 
     var body: some View {
         let spectrum = Gradient(colors: [.red, .yellow, .green, .blue, .purple, .red])
 
         let conic = AngularGradient(gradient: spectrum, center: .center, angle: .degrees(0))
 
-        return
-            GeometryReader { (geometry: GeometryProxy) in
-                    Circle()
-                        .size(geometry.size)
-                        .fill(conic)
-                        .overlay(self.generateHandle(geometry: geometry))
-                }
-    }
-
-    /// Prevent values like hue, saturation and brightness from being greater than 1 or less than 0
-    func limitValue(_ value: Double, _ limit: CGFloat, _ state: CGFloat) -> Double {
-        max(0, min(1, value + Double(state / limit)))
+        return GeometryReader { (geometry: GeometryProxy) in
+            Circle()
+                .size(geometry.size)
+                .fill(conic)
+                .overlay(self.generateHandle(geometry: geometry))
+        }
     }
 
     /// Prevent the draggable element from going over its limit
@@ -81,19 +75,13 @@ struct ColorSelection: View {
     }
 
     /// Prevent the draggable element from going beyond the circle
-    func limitCircle(_ point: CGPoint, _ limit: CGSize, _ state: CGSize) -> (CGPoint) {
-        let newx = point.x * limit.width + state.width
-        let newy = point.y * limit.height + state.height
-
-        let x1 = newx - limit.width / 2
-        let y1 = -newy + limit.height / 2
+    func limitCircle(_ point: CGPoint, _ geometry: CGSize, _ state: CGSize) -> (CGPoint) {
+        let x1 = point.x + state.width - geometry.width / 2
+        let y1 = point.y + state.height - geometry.height / 2
         let theta = atan2(x1, y1)
         // Circle limit.width = limit.height
-        let radius = min(sqrt(x1 * x1 + y1 * y1), limit.width / 2)
-
-        os_log("Slider new x1, x2 = %g, %g", log: .default, type: .info, x1, y1)
-
-        return CGPoint(x: cos(theta) * radius + limit.width / 2, y: -sin(theta) * radius + limit.width / 2)
+        let radius = min(sqrt(x1 * x1 + y1 * y1), geometry.width / 2)
+        return CGPoint(x: sin(theta) * radius + geometry.width / 2, y: cos(theta) * radius + geometry.width / 2)
     }
 
     /// Creates the `Handle` and adds the drag gesture to it.
@@ -101,7 +89,7 @@ struct ColorSelection: View {
         ///  [Reference]: https://developer.apple.com/documentation/swiftui/gestures/composing_swiftui_gestures "Composing SwiftUI Gestures "
         let longPressDrag = LongPressGesture(minimumDuration: 0.05)
             .sequenced(before: DragGesture())
-            .updating($satBrightState) { value, state, _ in
+            .updating($thumb) { value, state, _ in
                 switch value {
                 // Long press begins.
                 case .first(true):
@@ -116,18 +104,20 @@ struct ColorSelection: View {
             }
             .onEnded { value in
                 guard case .second(true, let drag?) = value else { return }
-                self.saturation = self.limitValue(self.saturation, geometry.size.width, drag.translation.width)
-                self.brightness = self.limitValue(self.brightness, geometry.size.height, drag.translation.height)
+                os_log("Translation x y = %g, %g", log: .default, type: .info, drag.translation.width, drag.translation.height)
+
+                self.xpos += Double(drag.translation.width)
+                self.ypos += Double(drag.translation.height)
             }
 
         // MARK: Customize Handle Here
 
         // Add the gestures and visuals to the handle
         return Circle()
-            .overlay(satBrightState.isDragging ? Circle().stroke(Color.white, lineWidth: 2) : nil)
+            .overlay(thumb.isDragging ? Circle().stroke(Color.white, lineWidth: 2) : nil)
             .foregroundColor(.white)
             .frame(width: 25, height: 25, alignment: .center)
-            .position(limitCircle(CGPoint(x: saturation, y: brightness), geometry.size, satBrightState.translation))
+            .position(limitCircle(CGPoint(x: xpos, y: ypos), geometry.size, thumb.translation))
             .animation(.interactiveSpring())
             .gesture(longPressDrag)
     }
