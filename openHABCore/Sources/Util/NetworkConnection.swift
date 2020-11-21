5446ca0f0db264f866ba50251c8062af1c9e4df5
Al Bastien <al.bastien@gmail.com>
2020-11-21 14:34:22 -0500

    Added timeout configuration
    Signed-off-by: Al Bastien <ajbastien@users.noreply.github.com>

--- openHABCore/Sources/Util/NetworkConnection.swift
+++ openHABCore/Sources/Util/NetworkConnection.swift
@@ -97,28 +97,28 @@ public class NetworkConnection {
                                 deviceId: String,
                                 deviceName: String, completionHandler: @escaping (DataResponse<Data>) -> Void) {
         if let url = Endpoint.appleRegistration(prefsURL: prefsURL, deviceToken: deviceToken, deviceId: deviceId, deviceName: deviceName).url {
-            load(from: url, completionHandler: completionHandler)
+            load(from: url, timeout: nil, completionHandler: completionHandler)
         }
     }
 
     public static func sitemaps(openHABRootUrl: String,
                                 completionHandler: @escaping (DataResponse<Data>) -> Void) {
         if let url = Endpoint.sitemaps(openHABRootUrl: openHABRootUrl).url {
-            load(from: url, completionHandler: completionHandler)
+            load(from: url, timeout: nil, completionHandler: completionHandler)
         }
     }
 
     public static func tracker(openHABRootUrl: String,
                                completionHandler: @escaping (DataResponse<Data>) -> Void) {
         if let url = Endpoint.tracker(openHABRootUrl: openHABRootUrl).url {
-            load(from: url, completionHandler: completionHandler)
+            load(from: url, timeout: nil, completionHandler: completionHandler)
         }
     }
 
     public static func notification(urlString: String,
                                     completionHandler: @escaping (DataResponse<Data>) -> Void) {
         if let notificationsUrl = Endpoint.notification(prefsURL: urlString).url {
-            load(from: notificationsUrl, completionHandler: completionHandler)
+            load(from: notificationsUrl, timeout: nil, completionHandler: completionHandler)
         }
     }
 
@@ -211,9 +211,9 @@ public class NetworkConnection {
         return page(url: pageToLoadUrl, longPolling: longPolling, openHABVersion: openHABVersion, completionHandler: completionHandler)
     }
 
-    static func load(from url: URL, completionHandler: @escaping (DataResponse<Data>) -> Void) {
+    static func load(from url: URL, timeout: Double?, completionHandler: @escaping (DataResponse<Data>) -> Void) {
         var request = URLRequest(url: url)
-        request.timeoutInterval = 10.0
+        request.timeoutInterval = timeout ?? 10.0
 
         os_log("Firing request", log: .viewCycle, type: .debug)
         let task = NetworkConnection.shared.manager.request(request)
