49c7e066c2c5dde06b33ab27995c7ae3407695f9
weak <weak@fraglab.at>
2022-02-08 08:50:02 +0100

    send credentials for myopenhab.org subdomains
    Signed-off-by: weak <weak@fraglab.at>

--- OpenHABCore/Sources/OpenHABCore/Util/OpenHABAccessTokenAdapter.swift
+++ OpenHABCore/Sources/OpenHABCore/Util/OpenHABAccessTokenAdapter.swift
@@ -21,7 +21,7 @@ public class OpenHABAccessTokenAdapter: RequestInterceptor {
     }
 
     public func adapt(_ urlRequest: URLRequest, for session: Session, completion: @escaping (Result<URLRequest, Error>) -> Void) {
-        guard appData.openHABAlwaysSendCreds || urlRequest.url?.host == "myopenhab.org" else {
+        guard appData.openHABAlwaysSendCreds || urlRequest.url?.host?.hasSuffix("myopenhab.org") == true else {
             // The user did not choose for the credentials to be sent with every request.
             return completion(.success(urlRequest))
         }
@@ -40,7 +40,7 @@ public class OpenHABAccessTokenAdapter: RequestInterceptor {
     }
 
     public func adapt(_ urlRequest: URLRequest) throws -> URLRequest {
-        guard appData.openHABAlwaysSendCreds || urlRequest.url?.host == "myopenhab.org" else {
+        guard appData.openHABAlwaysSendCreds || urlRequest.url?.host?.hasSuffix("myopenhab.org") == true else {
             // The user did not choose for the credentials to be sent with every request.
             return urlRequest
         }
