2097fb41b4f887fd61716921b6596ae2e7ce8bfe
weakfl <weak@fraglab.at>
2022-06-22 09:18:36 +0200

    update workflows (#674)
    * update GitHub actions
    * update build tools
    * update fastlane
    
    Signed-off-by: weak <weak@fraglab.at>

--- BuildTools/Package.swift
+++ BuildTools/Package.swift
@@ -3,6 +3,7 @@ import PackageDescription
 
 let package = Package(
     name: "BuildTools",
+    platforms: [.macOS(.v10_12)],
     dependencies: [
         // Define any tools you want available from your build phases
         // Here's an example with SwiftFormat
