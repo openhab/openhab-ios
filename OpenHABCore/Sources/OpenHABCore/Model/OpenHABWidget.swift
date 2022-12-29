691670b7260083259afe37d9d963c3454259955e
Tim Bert <5411131+timbms@users.noreply.github.com>
2022-12-29 16:17:01 +0100

    refs #609 (#675)

--- OpenHABCore/Sources/OpenHABCore/Model/OpenHABWidget.swift
+++ OpenHABCore/Sources/OpenHABCore/Model/OpenHABWidget.swift
@@ -108,7 +108,7 @@ public class OpenHABWidget: NSObject, MKAnnotation, Identifiable {
     // Text between square brackets
     public var labelValue: String? {
         // Swift 5 raw strings
-        let regex = try? NSRegularExpression(pattern: #"\[(.*?)\]"#, options: [])
+        let regex = try? NSRegularExpression(pattern: #"\[(.*?)\]"#, options: [.dotMatchesLineSeparators])
         guard let match = regex?.firstMatch(in: label, options: [], range: NSRange(location: 0, length: (label as NSString).length)) else { return nil }
         guard let range = Range(match.range(at: 1), in: label) else { return nil }
         return String(label[range])
