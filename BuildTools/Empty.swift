1f35f1840c44baf7d506800f006ca44e68d5ac1c
Tim Bert <5411131+timbms@users.noreply.github.com>
2024-01-15 09:58:11 +0100

    Update copyright notice, second batch Reverted .swiftformat to consider --header
    Signed-off-by: Tim Bert <5411131+timbms@users.noreply.github.com>

--- BuildTools/Empty.swift
+++ BuildTools/Empty.swift
@@ -1,4 +1,4 @@
-// Copyright (c) 2010-2023 Contributors to the openHAB project
+// Copyright (c) 2010-2024 Contributors to the openHAB project
 //
 // See the NOTICE file(s) distributed with this work for additional
 // information.
@@ -8,3 +8,4 @@
 // http://www.eclipse.org/legal/epl-2.0
 //
 // SPDX-License-Identifier: EPL-2.0
+
