19463c9691ff1aed0e9caefa626ea2cd0cfd9443
Tim Müller-Seydlitz <timbms@gmail.com>
2024-08-30 13:33:59 +0200

    Preparing to make OpenHABWidget an ObservableObject Getting the separatorInset back Reverting diffable data source to work on OpenHABWidget instead of OpenHABWidget.ID

---
+++ openHAB/FrameCellView.swift
@@ -0,0 +1,19 @@
+//
+//  FrameCellView.swift
+//  openHAB
+//
+//  Created by Tim on 29.08.24.
+//  Copyright © 2024 openHAB e.V. All rights reserved.
+//
+
+import SwiftUI
+
+struct FrameCellView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FrameCellView()
+}
