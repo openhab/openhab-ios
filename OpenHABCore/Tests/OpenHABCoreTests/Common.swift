3af10b3f8347089a666e5a090acf3c88d3d61147
Tim Bert <5411131+timbms@users.noreply.github.com>
2025-03-09 07:28:55 +0100

    Clean up on test
    Signed-off-by: Tim Bert <5411131+timbms@users.noreply.github.com>

--- OpenHABCore/Tests/OpenHABCoreTests/Common.swift
+++ OpenHABCore/Tests/OpenHABCoreTests/Common.swift
@@ -54,23 +54,20 @@ public extension Date {
 }
 
 public extension HTTPResponse {
-    func withEncodedBody(_ encodedBody: String) throws -> (HTTPResponse, HTTPBody) { (self, .init(encodedBody)) }
-
-    static var listPetsSuccess: (HTTPResponse, HTTPBody) {
+    static var listRootSuccess: (HTTPResponse, HTTPBody) {
         get throws {
+            // swiftlint:disable line_length
             try Self(status: .ok, headerFields: [.contentType: "application/json"])
                 .withEncodedBody(
                     #"""
-                    [
-                      {
-                        "id": 1,
-                        "name": "Fluffz"
-                      }
-                    ]
+                    {"version":"8","locale":"en_DE","measurementSystem":"SI","runtimeInfo":{"version":"4.3.2","buildString":"Release Build"},"links":[{"type":"config-descriptions","url":"http://192.168.2.10:8080/rest/config-descriptions"},{"type":"auth","url":"http://192.168.2.10:8080/rest/auth"},{"type":"habpanel","url":"http://192.168.2.10:8080/rest/habpanel"},{"type":"sitemaps","url":"http://192.168.2.10:8080/rest/sitemaps"},{"type":"persistence","url":"http://192.168.2.10:8080/rest/persistence"},{"type":"addons","url":"http://192.168.2.10:8080/rest/addons"},{"type":"things","url":"http://192.168.2.10:8080/rest/things"},{"type":"channel-types","url":"http://192.168.2.10:8080/rest/channel-types"},{"type":"profile-types","url":"http://192.168.2.10:8080/rest/profile-types"},{"type":"module-types","url":"http://192.168.2.10:8080/rest/module-types"},{"type":"links","url":"http://192.168.2.10:8080/rest/links"},{"type":"thing-types","url":"http://192.168.2.10:8080/rest/thing-types"},{"type":"tags","url":"http://192.168.2.10:8080/rest/tags"},{"type":"discovery","url":"http://192.168.2.10:8080/rest/discovery"},{"type":"events","url":"http://192.168.2.10:8080/rest/events"},{"type":"rules","url":"http://192.168.2.10:8080/rest/rules"},{"type":"services","url":"http://192.168.2.10:8080/rest/services"},{"type":"items","url":"http://192.168.2.10:8080/rest/items"},{"type":"actions","url":"http://192.168.2.10:8080/rest/actions"},{"type":"logging","url":"http://192.168.2.10:8080/rest/logging"},{"type":"audio","url":"http://192.168.2.10:8080/rest/audio"},{"type":"voice","url":"http://192.168.2.10:8080/rest/voice"},{"type":"templates","url":"http://192.168.2.10:8080/rest/templates"},{"type":"inbox","url":"http://192.168.2.10:8080/rest/inbox"},{"type":"systeminfo","url":"http://192.168.2.10:8080/rest/systeminfo"},{"type":"ui","url":"http://192.168.2.10:8080/rest/ui"},{"type":"transformations","url":"http://192.168.2.10:8080/rest/transformations"},{"type":"uuid","url":"http://192.168.2.10:8080/rest/uuid"},{"type":"spec","url":"http://192.168.2.10:8080/rest/spec"},{"type":"iconsets","url":"http://192.168.2.10:8080/rest/iconsets"}]}
                     """#
                 )
+            // swiftlint:enable line_length
         }
     }
+
+    func withEncodedBody(_ encodedBody: String) throws -> (HTTPResponse, HTTPBody) { (self, .init(encodedBody)) }
 }
 
 public extension Data {
