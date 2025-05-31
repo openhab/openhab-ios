ecb5f54985b6ed3dd35c650fd5300321b6cfbede
Tassilo Karge <tassilo.karge@web.de>
2025-05-31 09:36:05 +0200

    add option for home selection to drawer menu
    Signed-off-by: Tassilo Karge <tassilo.karge@web.de>

--- openHAB/DrawerView.swift
+++ openHAB/DrawerView.swift
@@ -81,24 +81,16 @@ struct ConnectionView: View {
 }
 
 struct DrawerView: View {
-    struct MainSectionView: View {
-        var openHABIconwidth: CGFloat
-        var onDismiss: (TargetController) -> Void
-        var dismiss: DismissAction
+    struct MainSectionView<MenuEntry: View>: View {
+        var menuEntry: (Image, Text, TargetController) -> MenuEntry
 
         var body: some View {
             Section(header: Text("Main")) {
-                HStack {
-                    Image("openHABIcon")
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(width: openHABIconwidth)
-                    Text("Home")
-                }
-                .onTapGesture {
-                    dismiss()
-                    onDismiss(.webview)
-                }
+                menuEntry(
+                    Image("openHABIcon"),
+                    Text("Home"),
+                    .webview
+                )
             }
         }
     }
@@ -200,39 +192,27 @@ struct DrawerView: View {
         }
     }
 
-    struct SystemSectionView: View {
-        var openHABIconwidth: CGFloat
-        var onDismiss: (TargetController) -> Void
-        var dismiss: DismissAction
+    struct SystemSectionView<MenuEntry: View>: View {
+        var menuEntry: (Image, Text, TargetController) -> MenuEntry
 
         var body: some View {
             Section(header: Text("System")) {
-                HStack {
-                    Image(systemSymbol: .gear)
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(width: openHABIconwidth)
-                    Text(LocalizedStringKey("settings"))
-                }
-                .onTapGesture {
-                    dismiss()
-                    onDismiss(.settings)
-                }
+                settingsMenuEntry(image: .gear, text: "settings", goTo: .settings)
 
                 if Preferences.getLowestPriorityOpenHABConnection() != nil, !Preferences.demomode {
-                    HStack {
-                        Image(systemSymbol: .bell)
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .frame(width: openHABIconwidth)
-                        Text(LocalizedStringKey("notifications"))
-                    }
-                    .onTapGesture {
-                        dismiss()
-                        onDismiss(.notifications)
-                    }
+                    settingsMenuEntry(image: .bell, text: "notifications", goTo: .notifications)
                 }
+
+                settingsMenuEntry(image: .house, text: "homeSelection", goTo: .homeSelection)
             }
+        }
+
+        private func settingsMenuEntry(image: SFSymbol, text: String, goTo target: TargetController) -> MenuEntry {
+            menuEntry(
+                Image(systemSymbol: image),
+                Text(LocalizedStringKey(text)),
+                target
+            )
         }
     }
 
@@ -255,13 +235,13 @@ struct DrawerView: View {
     var body: some View {
         VStack {
             List {
-                MainSectionView(openHABIconwidth: openHABIconwidth, onDismiss: onDismiss, dismiss: dismiss)
+                MainSectionView(menuEntry: menuEntry)
 
                 TilesSectionView(uiTiles: uiTiles, tilesIconwidth: tilesIconwidth, onDismiss: onDismiss, dismiss: dismiss)
 
                 SitemapsSectionView(sitemaps: sitemaps, sitemapIconwidth: sitemapIconwidth, sitemapForWatch: $sitemapForWatch, onDismiss: onDismiss, dismiss: dismiss)
 
-                SystemSectionView(openHABIconwidth: openHABIconwidth, onDismiss: onDismiss, dismiss: dismiss)
+                SystemSectionView(menuEntry: menuEntry)
             }
             .listStyle(.inset)
 
@@ -279,6 +259,20 @@ struct DrawerView: View {
             Task {
                 await updateSitemapsAndUITiles(activeConnection: activeConnection)
             }
+        }
+    }
+
+    private func menuEntry(image: Image, text: Text, goTo target: TargetController) -> some View {
+        HStack {
+            image
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: openHABIconwidth)
+            text
+        }
+        .onTapGesture {
+            dismiss()
+            onDismiss(target)
         }
     }
 
