09f1777df0ddb540592e94c2845f05cbdaa5b697
Dan Cunningham <dan@digitaldan.com>
2025-06-07 10:49:07 -0700

    Enable custom certs again
    Signed-off-by: Dan Cunningham <dan@digitaldan.com>

--- OpenHABCore/Sources/OpenHABCore/Util/ServerCertificateManager.swift
+++ OpenHABCore/Sources/OpenHABCore/Util/ServerCertificateManager.swift
@@ -35,7 +35,7 @@ public class ServerCertificateManager { // ServerTrustManager, ServerTrustEvalua
         case permitAlways
     }
 
-    weak var delegate: (any ServerCertificateManagerDelegate)?
+    public weak var delegate: (any ServerCertificateManagerDelegate)?
     // ignoreSSL is a synonym for allowInvalidCertificates, ignoreCertificates
     public var ignoreSSL = false
     public var trustedCertificates: [String: Data] = [:]
