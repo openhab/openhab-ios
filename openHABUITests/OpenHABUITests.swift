9389938a60dc0925d6e190ce67aa31f290ab498c
Tim Mueller-Seydlitz <timbms@gmail.com>
2025-08-04 18:23:18 +0200

    Removed auto-generated menu tokens and extended tests to show new server entry
    Signed-off-by: Tim Mueller-Seydlitz <timbms@gmail.com>

--- openHABUITests/OpenHABUITests.swift
+++ openHABUITests/OpenHABUITests.swift
@@ -13,6 +13,7 @@ import os.log
 import XCTest
 
 class OpenHABUITests: XCTestCase {
+    let runWebViewAndSitemap = true // To accelerate testing of settings set to false
     override func setUp() async throws {
         try await super.setUp()
 
@@ -38,64 +39,68 @@ class OpenHABUITests: XCTestCase {
         hamburgerButton.tap()
         sleep(3)
 
-        app.staticTexts["Home"].tap()
-        sleep(10)
-        snapshot("0_MainUI")
+        if runWebViewAndSitemap {
+            app.staticTexts["Home"].tap()
+            sleep(10)
+            snapshot("0_MainUI")
 
-        // Locations Tab
-        let webViewsQuery = app.webViews.webViews.webViews
-        webViewsQuery.links["placemark_fill Locations"].tap()
-        sleep(2)
-        snapshot("1_Locations")
-
-        webViewsQuery.staticTexts["Living Room"].tap()
-        sleep(2)
-        snapshot("2_LivingRoom")
-        // Close button on Living Room view
-        webViewsQuery.otherElements["openHAB"].children(matching: .link).matching(identifier: "multiply_circle_fill").element(boundBy: 0).staticTexts["multiply_circle_fill"].tap()
-        sleep(2)
-
-        var menuStaticText: XCUIElement?
-        // if we have a left side menu, then use it (large screens like 12.9 inch iPad will not)
-        if !webViewsQuery.staticTexts["Floorplans"].exists {
-            // Left side menu in webUI
-            menuStaticText = webViewsQuery.staticTexts["menu"]
+            // Locations Tab
+            let webViewsQuery = app.webViews.webViews.webViews
+            webViewsQuery.links["placemark_fill Locations"].tap()
             sleep(2)
+            snapshot("1_Locations")
+
+            webViewsQuery.staticTexts["Living Room"].tap()
+            sleep(2)
+            snapshot("2_LivingRoom")
+            // Close button on Living Room view
+            webViewsQuery.otherElements["openHAB"].children(matching: .link).matching(identifier: "multiply_circle_fill").element(boundBy: 0).staticTexts["multiply_circle_fill"].tap()
+            sleep(2)
+
+            var menuStaticText: XCUIElement?
+            // if we have a left side menu, then use it (large screens like 12.9 inch iPad will not)
+            if !webViewsQuery.staticTexts["Floorplans"].exists {
+                // Left side menu in webUI
+                menuStaticText = webViewsQuery.staticTexts["menu"]
+                sleep(2)
+                menuStaticText?.tap()
+                sleep(1)
+            }
+
+            app.staticTexts["Floorplans"].tap()
+            sleep(10)
+            snapshot("3_Floorplans")
+
             menuStaticText?.tap()
             sleep(1)
+            // openHAB logo in left menu
+            webViewsQuery.links.allElementsBoundByIndex[1].tap()
+            sleep(2)
+
+            app.webViews.staticTexts["square_arrow_right"].tap()
+
+            app.staticTexts["Main Menu"].tap()
+            app.cells.containing(.staticText, identifier: "Widget Overview").firstMatch.tap()
+            sleep(10)
+            snapshot("4_MainSitemap")
+
+            app.staticTexts["BINARY WIDGETS"].swipeUp()
+            sleep(6)
+            snapshot("5_WidgetOverview")
+
+            app.navigationBars.buttons.element(boundBy: 0).tap()
+            sleep(2)
+
+            hamburgerButton.tap()
+            sleep(3)
         }
-
-        app/*@START_MENU_TOKEN@*/ .staticTexts["Floorplans"]/*[[".links[\"Floorplans\"].staticTexts.firstMatch",".links.staticTexts[\"Floorplans\"]",".staticTexts[\"Floorplans\"]"],[[[-1,2],[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/ .tap()
-        sleep(10)
-        snapshot("3_Floorplans")
-
-        menuStaticText?.tap()
-        sleep(1)
-        // openHAB logo in left menu
-        webViewsQuery.links.allElementsBoundByIndex[1].tap()
-        sleep(2)
-
-        app.webViews/*@START_MENU_TOKEN@*/ .staticTexts["square_arrow_right"]/*[[".links[\"square_arrow_right\"].staticTexts.firstMatch",".links.staticTexts[\"square_arrow_right\"]",".staticTexts[\"square_arrow_right\"]"],[[[-1,2],[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/ .tap()
-
-        app/*@START_MENU_TOKEN@*/ .staticTexts["Main Menu"]/*[[".otherElements.staticTexts[\"Main Menu\"]",".staticTexts[\"Main Menu\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/ .tap()
-        app.cells/*@START_MENU_TOKEN@*/ .containing(.staticText, identifier: "Widget Overview").firstMatch/*[[".element(boundBy: 11)",".containing(.staticText, identifier: \"Widget Overview\").firstMatch"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/ .tap()
-        sleep(10)
-        snapshot("4_MainSitemap")
-
-        app.staticTexts["BINARY WIDGETS"].swipeUp()
-        sleep(6)
-        snapshot("5_WidgetOverview")
-
-        app.navigationBars.buttons.element(boundBy: 0).tap()
-        sleep(2)
-
-        hamburgerButton.tap()
-        sleep(3)
         app.staticTexts["settings"].tap()
         sleep(2)
         snapshot("7_Settings_Demo")
 
-        app.staticTexts["Demo Mode"].tap()
+//        let switch1 = app.switches["Demo Mode"]
+        let switch1 = app.switches["1"]
+        switch1.tap()
         sleep(2)
         snapshot("8_Settings_Server")
     }
