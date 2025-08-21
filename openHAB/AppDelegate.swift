23e64f7c4b9389f0140eebd5319c816a4c5138b3
Tim Mueller-Seydlitz <timbms@gmail.com>
2025-08-21 09:47:19 +0200

    Revert "Add missing git configuration for workflow"
    This reverts commit e991ce59bb7c40bb1804589f9bb39c1cb5213061.

--- openHAB/AppDelegate.swift
+++ openHAB/AppDelegate.swift
@@ -320,7 +320,6 @@ extension AppDelegate: UNUserNotificationCenterDelegate {
     }
 
     // Action to be performed when the notification message view is tapped
-    @MainActor
     func messageViewTapped(action: String?, cloudUserId: String? = nil) {
         notifyNotificationListeners(action: action, cloudUserId: cloudUserId)
         SwiftMessages.hideAll()
