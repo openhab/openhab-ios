a4f68cdeb4c1c815a588122827d1f9a7c12f2278
Tim Bert <5411131+timbms@users.noreply.github.com>
2026-01-17 20:05:11 +0100

    Fix/1038 auto back from selection view (#1041)
    * Automatic navigation back to OpenHABSitemapViewController after selecting an option in SelectionView. Fix #1038
    * preserve scroll position by not stopping tasks when navigating to SelectionView
    * Preserve scroll position when returning from SelectionView also on a linkedPage
    Keep polling task alive during SelectionView navigation to prevent table reload.
    
    ---------
    
    Signed-off-by: Tim Mueller-Seydlitz <timbms@gmail.com>

--- openHAB/OpenHABSitemapViewController.swift
+++ openHAB/OpenHABSitemapViewController.swift
@@ -43,7 +43,7 @@ class OpenHABSitemapViewController: OpenHABViewController, UISearchControllerDel
     private let searchController = UISearchController(searchResultsController: nil)
     private var isUserInteracting = false
     private var isWaitingToReload = false
-    // Properties in your view controller:
+    private var isNavigatingToSelection = false
 
     private var pageHandlingTask: Task<Void, Never>?
 
@@ -163,18 +163,21 @@ class OpenHABSitemapViewController: OpenHABViewController, UISearchControllerDel
                 widgetTableView.reloadData()
             }
             Logger.sitemapViewController.info("OpenHABSitemapViewController pageUrl is empty, this is first launch")
+            startWatchingActiveServer()
         } else {
-            if !pageNetworkStatusChanged() || !pageId.isEmpty {
+            // Skip restarting if polling task is still active (e.g., returning from SelectionView)
+            if let task = pageHandlingTask, !task.isCancelled {
+                Logger.sitemapViewController.info("OpenHABSitemapViewController polling still active, skipping restart")
+            } else if !pageNetworkStatusChanged() || !pageId.isEmpty {
                 // swiftformat:disable:next redundantSelf
                 Logger.sitemapViewController.info("OpenHABSitemapViewController pageUrl \(self.pageUrl)")
                 startPageHandling()
+                startWatchingActiveServer()
             } else {
                 Logger.sitemapViewController.info("OpenHABSitemapViewController network status changed while it was not appearing")
                 restart()
             }
         }
-
-        startWatchingActiveServer()
 
         ImageDownloader.default.authenticationChallengeResponder = self
     }
@@ -183,7 +186,11 @@ class OpenHABSitemapViewController: OpenHABViewController, UISearchControllerDel
         Logger.sitemapViewController.info("OpenHABSitemapViewController viewWillDisappear")
 
         trackerCancellables.removeAll()
-        stopAllTasks()
+        // Keep polling alive when pushing to SelectionView to preserve scroll position
+        if !isNavigatingToSelection {
+            stopAllTasks()
+        }
+        isNavigatingToSelection = false
 
         super.viewWillDisappear(animated)
 
@@ -801,9 +808,12 @@ extension OpenHABSitemapViewController: UITableViewDelegate, UITableViewDataSour
                 ) { selectedMappingIndex in
                     let selectedMapping: OpenHABWidgetMapping = widget.mappingsOrItemOptions[selectedMappingIndex]
                     self.sendCommand(widget.item, commandToSend: selectedMapping.command)
+                } onDismiss: { [weak self] in
+                    self?.navigationController?.popViewController(animated: true)
                 }
             )
             hostingController.title = widget.labelText
+            isNavigatingToSelection = true
             navigationController?.pushViewController(hostingController, animated: true)
         } else if widget.type == .input {
             let hint = widget.inputHint
