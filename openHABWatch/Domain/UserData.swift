c7a53a7f37cd276234f1111dd37526535a427460
Tim Mueller-Seydlitz <timbms@gmail.com>
2026-02-22 16:32:59 +0100

    watchOS: remove duplicate decorateWidgetsWithSendCommand introduced by merge
    Signed-off-by: Tim Mueller-Seydlitz <timbms@gmail.com>

--- openHABWatch/Domain/UserData.swift
+++ openHABWatch/Domain/UserData.swift
@@ -466,13 +466,4 @@ final class UserData: ObservableObject {
         }
     }
 
-    /// Sets sendCommand closures on widgets that go directly to UserData,
-    /// bypassing the OpenHABPage closure chain and its weak-reference lifetime issues.
-    private func decorateWidgetsWithSendCommand(_ widgets: [OpenHABWidget]) {
-        for widget in widgets {
-            widget.sendCommand = { [weak self] item, command in
-                Task { await self?.sendCommand(item, command: command) }
-            }
-        }
-    }
 }
