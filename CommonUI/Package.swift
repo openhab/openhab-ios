4884040eb06db2485944d81ab07743e4e29e8d27
Tim Mueller-Seydlitz <timbms@gmail.com>
2026-04-18 09:22:31 +0200

    Add SFSafeSymbols dependency to CommonUI
    Allows CommonUI to use type-safe symbol references instead of
    raw string-based Image(systemName:) calls.
    
    Signed-off-by: Tim Mueller-Seydlitz <timbms@gmail.com>

--- CommonUI/Package.swift
+++ CommonUI/Package.swift
@@ -16,14 +16,15 @@ let package = Package(
     ],
     dependencies: [
         .package(path: "../OpenHABCore"),
-        .package(url: "https://github.com/apple/swift-numerics", from: "1.0.0")
+        .package(url: "https://github.com/apple/swift-numerics", from: "1.0.0"),
+        .package(url: "https://github.com/SFSafeSymbols/SFSafeSymbols", from: "7.0.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "CommonUI",
-            dependencies: ["OpenHABCore"],
+            dependencies: ["OpenHABCore", .product(name: "SFSafeSymbols", package: "SFSafeSymbols")],
             swiftSettings: [
                 .enableUpcomingFeature("ExistentialAny"),
                 //                .enableUpcomingFeature("InternalImportsByDefault"),
